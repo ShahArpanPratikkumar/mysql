c43f6a521b3f17c843f2ccbbcdd93c7d3f0aea13
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-05 13:26:44 +0530

    Time: 58 ms (47.11%), Space: 24 MB (26.24%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -14,14 +14,11 @@ public:
         ListNode *pA = headA;
         ListNode *pB = headB;
 
-        // Traverse both lists. When either pointer reaches the end (nullptr),
-        // redirect it to the head of the other list.
         while (pA != pB) {
             pA = (pA == nullptr) ? headB : pA->next;
             pB = (pB == nullptr) ? headA : pB->next;
         }
 
-        // pA (or pB) will either be the intersection node or nullptr if no intersection exists
         return pA;
     }
 };
