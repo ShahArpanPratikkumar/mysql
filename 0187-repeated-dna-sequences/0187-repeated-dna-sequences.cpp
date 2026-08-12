6880df5515d7e22ba992e9952a34ec031dbdc18e
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-12 14:26:01 +0530

    Time: 1265 ms (5.13%), Space: 375.7 MB (6.1%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_set<string> seen;
+        unordered_set<string> repeated;
+        
+        if (s.length() < 10) {
+            return {};
+        }
+        
+        for (int i = 0; i <= static_cast<int>(s.length()) - 10; ++i) {
+            string sub = s.substr(i, 10);
+            if (seen.count(sub)) {
+                repeated.insert(sub);
+            } else {
+                seen.insert(sub);
+            }
+        }
+        
+        return vector<string>(repeated.begin(), repeated.end());
+    }
+};
